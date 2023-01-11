382b18f5342335f7b96836a68fae5af4a7c0c79d
merajuni7ed <merajmusavi1243m@gmail.com>
2023-01-11 20:47:52 +0330

    save state

--- app/src/main/java/com/example/xo/MainActivity.java
+++ app/src/main/java/com/example/xo/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.xo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+private int xOrO = 0;
+// 2 is empty state
+private int[] state = {2,2,2,2,2,2,2,2,2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onImageClick(View view){
+     ImageView imageView = (ImageView) view;
+     int tag_save = Integer.parseInt(imageView.getTag().toString());
+     state[tag_save] = xOrO;
+     imageView.setTranslationY(-1500);
+     if (xOrO==0){
+         imageView.setImageResource(R.drawable.x);
+          xOrO = 1;
+     }else{
+         imageView.setImageResource(R.drawable.oo);
+         xOrO = 0;
+
+     }
+     imageView.animate().translationYBy(1500).rotation(3600).setDuration(400);
+     }
 }
