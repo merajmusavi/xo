0bcecffdae24160bca8b9fdde7c75edd088b07e7
merajuni7ed <merajmusavi1243m@gmail.com>
2023-01-12 12:47:49 +0330

    initialize winner WITH BETTER WAY

--- app/src/main/java/com/example/xo/MainActivity.java
+++ app/src/main/java/com/example/xo/MainActivity.java
@@ -11,14 +11,10 @@ public class MainActivity extends AppCompatActivity {
 private int xOrO = 0;
 // 2 is empty state
 public int[] state = {2,2,2,2,2,2,2,2,2};
-private final int[] winP1 = {0,1,2};
-private final int[] winP2 = {3,4,5};
-private final int[] winP3 = {6,7,8};
-private final int[] winP4 = {0,4,8};
-private final int[] winP5 = {2,4,6};
-private final int[] winP6 = {0,3,6};
-private final int[] winP7 = {2,5,8};
-private final int[] winP8 = {0,3,6};
+private final int[][] winP = {{0,1,2},{3,4,5},{6,7,8},{0,4,8},{2,4,6},{0,3,6},{2,5,8},{0,3,6}};
+
+
+
 
 
     @Override
@@ -40,23 +36,11 @@ private final int[] winP8 = {0,3,6};
 
      }
      imageView.animate().translationYBy(1500).rotation(3600).setDuration(400);
-     if (state[winP1[0]]==state[winP1[1]] && state[winP1[0]]==state[winP1[2]] && state[0]!=2){
-         Toast.makeText(this,"win",Toast.LENGTH_SHORT).show();
-     }else if (state[winP2[0]]==state[winP2[1]] && state[winP2[0]]==state[winP2[2]] && state[3]!=2){
-         Toast.makeText(this, "win", Toast.LENGTH_SHORT).show();
-     }else if (state[winP3[0]]==state[winP3[1]]&& state[winP3[0]]==state[winP3[2]] && state[6]!=2){
-         Toast.makeText(this, "win", Toast.LENGTH_SHORT).show();
-     }else if (state[winP4[0]]==state[winP4[1]] && state[winP4[0]]==state[winP4[2]] && state[4]!=2){
-         Toast.makeText(this, "win", Toast.LENGTH_SHORT).show();
-     }else if (state[winP5[0]]==state[winP5[1]] && state[winP5[0]]==state[winP5[2]] && state[4]!=2){
-         Toast.makeText(this, "win", Toast.LENGTH_SHORT).show();
-     }else if(state[winP6[0]]==state[winP6[1]] && state[winP6[0]]==state[winP6[2]] && state[3]!=2){
-         Toast.makeText(this, "win", Toast.LENGTH_SHORT).show();
-     }else if (state[winP7[0]]==state[winP7[1]] && state[winP7[0]]==state[winP7[2]] && state[5]!=2){
-         Toast.makeText(this, "win", Toast.LENGTH_SHORT).show();
-     }else if (state[winP8[0]]==state[winP8[1]] && state[winP8[0]]==state[winP8[2]] && state[3]!=2){
-            Toast.makeText(this, "win", Toast.LENGTH_SHORT).show();
-     }
+        for (int[] winPosition : winP) {
+            if (state[winPosition[0]]==state[winPosition[1]] && state[winPosition[0]]==state[winPosition[2]] && state[winPosition[0]]!=2){
+                Toast.makeText(this, "WIN", Toast.LENGTH_SHORT).show();
+            }
+        }
      
      }
 }
